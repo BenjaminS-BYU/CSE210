e107efd5e8f1eac89ab302ddc4b18d8a56eed50a
BenjaminS-BYU <bstrong2@byupathway.edu>
2025-10-27 10:35:50 -0600

    Start of exercise two

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,39 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("What are your grades? ");
+        string userInput = Console.ReadLine();
+
+        int grades = int.Parse(userInput);
+
+        if (grades >= 90)
+        {
+            Console.WriteLine("You have an A");
+        }
+        else if (grades < 90 && grades >= 80)
+        {
+            Console.WriteLine("You have an B");
+        }
+        else if (grades < 80 && grades >= 70)
+        {
+            Console.WriteLine("You have an C");
+        }
+        else if (grades < 70 && grades >= 60)
+        {
+            Console.WriteLine("You have an D");
+        }
+        else
+        {
+            Console.WriteLine("You have an F");
+        }
+        if (grades > 70)
+        {
+            Console.WriteLine("You Passed");
+        }
+        else
+        {
+            Console.WriteLine("You failed");
+        }
     }
 }
