0625228dbd754f58ffeccd71f738d9cfeb34a4a2
BenjaminS-BYU <bstrong2@byupathway.edu>
2025-11-02 06:12:58 -0700

    started the main program

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-    //    Do magical stuff here
+        string usersChoice = "";
+        while (usersChoice != "5")
+        {
+            Console.Write(
+@"Welcome to the Journal Program!
+Please select one of the following choices:
+1. Write
+2. Display
+3. Load
+4. Save
+5. Quit
+What would you like to do? ");
+            usersChoice = Console.ReadLine();
+
+            if (usersChoice == "5")
+            {
+                Console.WriteLine("Goodbye.");
+                Environment.Exit(0);
+            }
+        }
     }
 }
