11a7e7ed52b1ee9a06e37eb3a1b6dbe1090d2af9
BenjaminS-BYU <bstrong2@byupathway.edu>
2025-11-01 22:02:22 -0600

    erased what I just wrote

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,10 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
-        // Write a journal interface where the user is given a prompt from a list and then they can write
-        // The user can then save this into a file and review what is in that file
-        // Need at least one save file to save journal entries to
-        
+       
     }
 }
