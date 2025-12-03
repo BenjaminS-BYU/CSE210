9fbacdf3cebede06ac39079f7c1b17c7911b97ea
BenjaminS-BYU <bstrong2@byupathway.edu>
2025-12-03 10:06:19 -0700

    mapped out the main program

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -4,6 +4,55 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the EternalQuest Project.");
+        GoalManager manager = new();
+
+        bool running = true;
+        while(running)
+        {
+            Console.Clear();
+            Console.Write(@$"
+            Score: {manager.GetScore()}
+            1. Create Goal
+            2. List Goals
+            3. Record Event
+            4. Save
+            5. Load
+            6. Quit
+
+            Choose: 
+            ");
+            string input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "6":
+                Console.WriteLine("Thank you for using the goal tracker program. Goodbye.");
+                running = false;
+                break;
+
+                case "1":
+                    
+                    break;
+
+                case "2":
+                    manager.DisplayGoals();
+                    Console.ReadKey();
+                    break;
+
+                case "3":
+                    
+                    break;
+
+                case "4":
+                    
+                    break;
+
+                case "5":
+                    
+                    break;
+            }
+
+
+        }
     }
 }
